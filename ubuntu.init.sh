53a68b2642288c180083516ec337641da7ff4b69
Canjiang Lu <craftsfish@163.com>
2018-07-14 20:40:45 +0800

    +ubuntu.init.sh

---
+++ ubuntu.init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install vim git fcitx-config-gtk fcitx-frontend-gtk3 fcitx-googlepinyin fcitx-ui-classic postgresql python-pip numlockx
+sudo -H pip install --upgrade pip
+sudo -H pip install django
