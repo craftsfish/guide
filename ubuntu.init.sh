00b99f672c12f3471887aebf99acedc40355a0e9
Canjiang Lu <craftsfish@163.com>
2018-07-30 21:11:04 +0800

    trivial

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install vim git fcitx-config-gtk fcitx-frontend-gtk3 fcitx-googlepinyin fcitx-ui-classic postgresql python-pip numlockx
+sudo apt-get install vim git fcitx-config-gtk fcitx-frontend-gtk3 fcitx-googlepinyin fcitx-ui-classic postgresql python-pip numlockx python-psycopg2
 sudo -H pip install --upgrade pip
 sudo -H pip install django
