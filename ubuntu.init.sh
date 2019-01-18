7377cd586b679c38c406de7289d6c7265d701353
Canjiang Lu <craftsfish@163.com>
2019-01-18 21:43:26 +0800

    trivial

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install vim git fcitx-config-gtk fcitx-frontend-gtk3 fcitx-googlepinyin fcitx-ui-classic postgresql python-pip numlockx python-psycopg2
-sudo -H pip install --upgrade pip
-sudo -H pip install django
+#sudo -H pip install --upgrade pip
+#sudo -H pip install django
 
 #lcj     ALL=(ALL:ALL) NOPASSWD:ALL
