20936570a3c66a44cb5bd8d4035933a1aed5ddd0
craftsfish <craftsfish@163.com>
2019-12-18 14:13:55 +0800

    Update ubuntu.init.sh

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sudo apt-get install vim git fcitx-config-gtk fcitx-frontend-gtk3 fcitx-googlepinyin fcitx-ui-classic postgresql python-pip numlockx python-psycopg2
+sudo apt install vim git numlockx
+sudo apt install fcitx-googlepinyin
+#sudo apt install postgresql python-pip python-psycopg2
 #sudo -H pip install --upgrade pip
 #sudo -H pip install django
 #umask 002
+
+sudo apt install build-essential
+sudo apt install libncurses-dev
+sudo apt install cscope
 
 #lcj     ALL=(ALL:ALL) NOPASSWD:ALL
