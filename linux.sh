39f71454112ea193b15594cfdd671308f3b041b1
Canjiang Lu <craftsfish@163.com>
2020-07-10 22:16:30 +0800

    update

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#git clone https://mirrors.tuna.tsinghua.edu.cn/git/linux.git
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+git clone https://mirrors.tuna.tsinghua.edu.cn/git/linux.git
+#git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 #sudo apt install build-essential
 #sudo apt install libncurses-dev
 sudo apt install cscope
