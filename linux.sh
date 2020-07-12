bd930d3a2d73f03e603a4c9720fd83483748e1d2
Canjiang Lu <craftsfish@163.com>
2020-07-12 17:31:23 +0800

    update

--- linux.sh
+++ linux.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-git clone https://mirrors.tuna.tsinghua.edu.cn/git/linux.git
+#git clone https://mirrors.tuna.tsinghua.edu.cn/git/linux.git
 #git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 #sudo apt install build-essential
 #sudo apt install libncurses-dev
 sudo apt install cscope
 mkdir -p $HOME/.vim/plugin
-cd $HOME/.vim/plugin
-wget https://raw.githubusercontent.com/craftsfish/guide/master/cscope_maps.vim
-cd -
+cp cscope_maps.vim $HOME/.vim/plugin/
